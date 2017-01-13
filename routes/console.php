fb281861196bf269646e8c32b0148ed85880cf18
felixbvf <felixbvf@gmail.com>
2017-01-13 19:41:08 -0400

    add Command

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
-
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -16,3 +16,23 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('task1:pay {name=Invitado} {--back}', function($name) {
+    if($this->option('back')) {
+            $this->info("Welcome to back, $name!");
+    }
+    else{
+        $this->info("Welcome to Task1 Muserpol, $name!");
+    }
+
+})->describe('welcome a user to our project');
+
+Artisan::command('styde1:register', function() {
+    $name = $this->ask('Por favor coloca tu nombre');
+    $email = $this->ask('Por favor coloca tu email');
+    $password = $this->ask('Por favor coloca tu password');
+    User::create(compact('name','email','password'));
+    //Message to show
+    $this->info("El usuario $name <$email> fue registrado con exito !!");
+
+});
